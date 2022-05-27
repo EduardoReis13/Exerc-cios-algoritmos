2fb3e5302a1da2f4f0bddf58d69cade77e4e62be
Eduardo Reis Gindri Rodrigues <103642178+EduardoReis13@users.noreply.github.com>
2022-05-27 16:02:11 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int ano_nascimento;
+        printf("Digite seu ano de nascimento:\n ");
+        scanf("%d", &ano_nascimento);
+        
+    int ano_atual;
+        printf("Digite o ano atual:\n ");
+        scanf("%d", &ano_atual);
+        
+    int idade= ano_atual- ano_nascimento;
+        printf("sua idade eh:%d \n ", ano_atual- ano_nascimento);
+        
+    
+        
+
+
+
+}
