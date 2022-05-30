a6b4eee72f24653a40d0cfa05fc211cdc0aee4ec
Eduardo Reis Gindri Rodrigues <103642178+EduardoReis13@users.noreply.github.com>
2022-05-30 09:56:58 -0300

    Add files via upload

---
+++ Lista 7/main (3).c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{ float n1;
+    printf("Insira um numero real:\n ");
+    scanf("%f", &n1);
+
+    float n2;
+        printf("Insira outro numero real:\n ");
+        scanf("%f", &n2);
+
+        if(n1>0 && n2>0){
+            printf("Seus valores sao validos \n ");
+        }
+
+        else{
+            printf("Seus valores sao invalidos \n ");
+        }
+
+
+
+
+}
