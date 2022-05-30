755e4359e8f19905710f00e48897b145f16de749
Eduardo Reis Gindri Rodrigues <103642178+EduardoReis13@users.noreply.github.com>
2022-05-30 09:17:22 -0300

    Add files via upload

---
+++ Lista 7/main (1).c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{ float temperatura;
+  printf("Qual eh a temperatura:\n ");
+  scanf("%f", &temperatura);
+   
+   printf("Farenheit: %.1f\n ", temperatura*1.8+32);
+   
+   printf("Celsius: %.1f", temperatura);
+
+    
+}
