00894c97c0ff2c33cf24a3e21d5f99a561c6ae18
Eduardo Reis Gindri Rodrigues <103642178+EduardoReis13@users.noreply.github.com>
2022-05-30 08:59:40 -0300

    Add files via upload

---
+++ Lista 1/main.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{ int n1,soma, multiplicacao;
+   printf("Insira um numero:\n ");
+   scanf("%d",&n1);
+   
+  int n2;
+    printf("Insira outro numero:\n ");
+    scanf("%d",&n2);
+    soma=n1+n2;
+    multiplicacao= soma*n1;
+    
+    
+    printf("O resultado da soma eh:%d", multiplicacao);
+
+    
+}
