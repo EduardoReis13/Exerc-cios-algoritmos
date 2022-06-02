6a779b18d542ee8c479cfcdab07006bb8f58ec66
Eduardo Reis Gindri Rodrigues <103642178+EduardoReis13@users.noreply.github.com>
2022-06-02 15:16:11 -0300

    Add files via upload

---
+++ Lista 7/main.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int senha, tent;
+    
+    printf ("Digite a senha:\n ");
+    scanf("%d", &tent);
+    
+    senha= 1234;
+    
+    if (tent == senha)
+    {
+    printf("Sua senha esta correta ");
+    }
+    
+    else
+    {
+        printf("Invalida");
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
