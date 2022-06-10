30cc8aae21c3d075725222b02060d3ddb10e07e9
Eduardo Reis Gindri Rodrigues <103642178+EduardoReis13@users.noreply.github.com>
2022-06-10 14:28:25 -0300

    Create exercicio 02.c

---
+++ Lista exer. Laço de repetiçã/exercicio 02.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int idade;
+    int soma;
+    int cont;
+    
+    while(1)
+    {
+        printf("Digite uma idade:\n ");
+        scanf("%d", &idade);
+        
+            if (idade<=0)
+            {
+                break;
+                
+            }
+    }
+    
+         printf("Sua media entre as idades eh:\n %f", (float) soma/cont);
+
+
+
+
+}
