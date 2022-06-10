880433a4e406689f24c3c2844ec7ac337a75770b
Eduardo Reis Gindri Rodrigues <103642178+EduardoReis13@users.noreply.github.com>
2022-06-10 14:06:00 -0300

    Update exercicio 01.c

--- Lista exer. Laço de repetiçã/exercicio 01.c
+++ Lista exer. Laço de repetiçã/exercicio 01.c
@@ -1,1 +1,26 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int numero, cont, cont1;
+    
+        cont=0;
+        cont1=0;
+
+    while (cont<10)
+    {
+        printf("Digite um numero:\n ");
+         scanf("%d", &numero);
+         
+        if (numero>30 && numero<90);
+            
+            cont1= cont1+1;
+            cont ++;
+            
+            
+    }    
+        
+            printf("A quantidade de numeros entre 30 e 90 eh:\n %d", cont1);
+        
+}
