cfaf8fff32b468eba68b5749940c145d6073f57d
Eduardo Reis Gindri Rodrigues <103642178+EduardoReis13@users.noreply.github.com>
2022-06-13 09:28:20 -0300

    Add files via upload

---
+++ Lista exer. Laço de repetiçã/main.c
@@ -0,0 +1,27 @@
+
+
+#include<stdio.h>
+
+    void main () 
+{
+    int x, cont;
+    cont=0;
+    
+        printf ("entre com um numero:\n");
+         scanf ("%d", &x);
+            
+        while (x!=0) 
+    {   
+        cont= cont++;
+        printf ("entre com um numero:\n");
+         scanf ("%d", &x);
+    }
+
+}
+
+
+
+
+
+
+
