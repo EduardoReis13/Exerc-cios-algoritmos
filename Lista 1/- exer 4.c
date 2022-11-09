19d88412570c1e765fec3e5df6acdf332c57e65a
Eduardo Reis Gindri Rodrigues <103642178+EduardoReis13@users.noreply.github.com>
2022-11-09 15:21:49 -0300

    Delete Lista 1 directory

--- Lista 1/- exer 4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
- float temperatura;
-    printf("Digite a temperatura:\n ");
-        scanf("%f", &temperatura);
-    printf("Farenheit %f", temperatura*1.8+32);
-  }
